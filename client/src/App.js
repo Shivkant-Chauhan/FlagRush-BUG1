6cb6c753176a011308370d194c68b7b07d157d53
Shivkant Chauhan <shivkantchauhan2002@gmail.com>
2023-04-07 23:15:14 +0530

    bug1 - CTF

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import SignUp from './components/Signup';
+
+function App() {
+  return (
+    <div className="App">
+      <SignUp />
+    </div>
+  );
+}
+
+export default App;
